eb3d802648e0fee08717addbfba18933012fa263
renatalantos <renatalantos@gmail.com>
2021-10-04 17:02:20 +0000

    Replaced accidentally deleted code

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['codestar2021reni.herokuapp.com', ]
+ALLOWED_HOSTS = ['codestar2021reni.herokuapp.com', 'localhost']
 
 
 # Application definition
