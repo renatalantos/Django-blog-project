23e2fb631d3ec98f04e5c7f1a389608e4d1007d1
renatalantos <renatalantos@gmail.com>
2021-10-14 11:41:27 +0000

    Added model classes to admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -5,12 +5,12 @@ from django_summernote.admin import SummernoteModelAdmin
 
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
-    
-    prepopulated_fields = {'slug': ('title',)}
-    list_display = ('title', 'slug', 'content', 'created_on')
-    list_filter = ('status', 'created_on')
-    summernote_fields = ('content')
+
+    list_display = ('title', 'slug', 'status', 'created_on')
     search_fields = ['title', 'content']
+    list_filter = ('status', 'created_on')
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content',)
 
 
 @admin.register(Comment)
@@ -20,7 +20,5 @@ class CommentAdmin(admin.ModelAdmin):
     search_fields = ('name', 'email', 'body')
     actions = ['approve_comments']
 
-
     def approve_comments(self, request, queryset):
         queryset.update(approved=True)
-
--- blog/views.py
+++ blog/views.py
@@ -5,19 +5,19 @@ from .models import Post
 from .forms import CommentForm
 
 
-
 class PostList(generic.ListView):
     model = Post
-    queryset = Post.objects.filter(status=1).order_by('created_on')
-    template_name = 'index.html'
+    queryset = Post.objects.filter(status=1).order_by("-created_on")
+    template_name = "index.html"
     paginate_by = 6
 
 
 class PostDetail(View):
+
     def get(self, request, slug, *args, **kwargs):
         queryset = Post.objects.filter(status=1)
         post = get_object_or_404(queryset, slug=slug)
-        comments = post.comments.filter(approved=True).order_by('created_on')
+        comments = post.comments.filter(approved=True).order_by("-created_on")
         liked = False
         if post.likes.filter(id=self.request.user.id).exists():
             liked = True
@@ -29,21 +29,21 @@ class PostDetail(View):
                 "post": post,
                 "comments": comments,
                 "commented": False,
-                "liked": liked, 
+                "liked": liked,
                 "comment_form": CommentForm()
             },
         )
-
+    
     def post(self, request, slug, *args, **kwargs):
+
         queryset = Post.objects.filter(status=1)
         post = get_object_or_404(queryset, slug=slug)
-        comments = post.comments.filter(approved=True).order_by('created_on')
+        comments = post.comments.filter(approved=True).order_by("-created_on")
         liked = False
         if post.likes.filter(id=self.request.user.id).exists():
             liked = True
 
         comment_form = CommentForm(data=request.POST)
-
         if comment_form.is_valid():
             comment_form.instance.email = request.user.email
             comment_form.instance.name = request.user.username
@@ -51,7 +51,7 @@ class PostDetail(View):
             comment.post = post
             comment.save()
         else:
-            comment_form = CommentForm()    
+            comment_form = CommentForm()
 
         return render(
             request,
@@ -60,19 +60,19 @@ class PostDetail(View):
                 "post": post,
                 "comments": comments,
                 "commented": True,
-                "liked": liked, 
-                "comment_form": CommentForm()
+                "comment_form": comment_form,
+                "liked": liked
             },
         )
 
 
 class PostLike(View):
-    def post(self, request, slug):
+    
+    def post(self, request, slug, *args, **kwargs):
         post = get_object_or_404(Post, slug=slug)
-        
         if post.likes.filter(id=request.user.id).exists():
             post.likes.remove(request.user)
         else:
-            post.likes.add(request.user)    
+            post.likes.add(request.user)
 
         return HttpResponseRedirect(reverse('post_detail', args=[slug]))
